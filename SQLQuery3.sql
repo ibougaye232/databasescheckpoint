7d25fda563e55adf3c0e1640fb5011597d33ae2d
ibougaye232 <gayeibrahima2005@gmail.com>
2023-11-16 10:51:07 +0000

    checkpoint base de données DML

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -1,18 +1,8 @@
-create table customer(
-customer_id int primary key,
-customer_name varchar(20),
-customer_address varchar(20)
-)
-create table PRODUCT(
-product_id int primary key,
-product_name varchar(30),
-product_price int check(product_price>0)
-)
-create table orders(
-order_id int primary key,
-customer_id int,
-product_id int,
-quantity int,
-constraint fk_product_id foreign key(product_id) references PRODUCT(product_id)
-constraint fk_customer_id foreign key(customer_id) references customer(customer_id)
-)
+insert into customer values(1,'Ahmed', 'Tunisie');
+insert into customer values(2,'Coulibaly', 'Sénégal');
+insert into customer values(3,'Hasan', 'Egypte');
+insert into PRODUCT values(1,'cookie', 10);
+insert into PRODUCT values(2,'candy', 5.2);
+insert into orders values(1,1,2,3,'2023-01-02');
+insert into orders values(2,2,1,10,'2023-04-14');
+
